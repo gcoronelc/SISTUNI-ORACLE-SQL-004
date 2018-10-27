416121456d57660d8f282b538c6194c113a276c6
Gustavo Coronel <gcoronelc@gmail.com>
2018-10-27 16:56:22 -0500

    Clase 02

---
+++ Clase02/VALORES-NULOS.sql
@@ -0,0 +1,35 @@
+-- MANIPULACIÓN DE VALORES NULOS
+
+SELECT 5 + NULL FROM DUAL;
+
+SELECT '' AS ALGO FROM DUAL;
+
+
+-- 1
+-- Desarrollar una sentencia SELECT que permita averiguar 
+-- los cursos que aún no tienen profesor. 
+-- Esquema EDUCA.
+
+SELECT * FROM EDUCA.CURSO
+WHERE TRIM(CUR_PROFESOR) IS NULL;
+
+SELECT * FROM EDUCA.CURSO
+WHERE NVL(TRIM(CUR_PROFESOR),' ') = ' ';
+
+
+-- 2
+-- Desarrollar una sentencia SELECT para consultar el 
+-- ingreso total de cada empleado. 
+-- Esquema HR.
+
+
+-- 3
+-- Desarrollar una sentencia SELECT para consultar los 
+-- empleados cuyos ingresos totales es menor a 8,000.00. 
+-- Esquema HR.
+
+
+-- 4
+-- Consultar los empleados cuyo ingreso (salario + comisión) 
+-- supera los 3500.00. Esquema SCOTT y HR.
+
