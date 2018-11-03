a1f2288a93079f2413b1502fcdf8663ad667959c
Gustavo Coronel <gcoronelc@gmail.com>
2018-11-03 18:01:33 -0500

    Clase 03

--- Clase03/Work02.sql
+++ Clase03/Work02.sql
@@ -30,15 +30,45 @@ WHERE IDDEPARTAMENTO NOT IN
 -- que tienen el menor salario por departamento.
 -- Esquema RECURSOS.
 
-
-
-
-
-
+SELECT * 
+FROM recursos.EMPLEADO
+WHERE (IDDEPARTAMENTO,SUELDO) IN (
+	SELECT IDDEPARTAMENTO, min(SUELDO)
+	FROM recursos.EMPLEADO
+	GROUP BY IDDEPARTAMENTO
+);
 
 -- SUBCONSULTA COMO TABLA DERIVADA
 
 -- Ejemplo
+-- Esquema RECURSOS
+-- De cada departamento, se necesita saber 
+-- la cantidad de empleados y su planilla.
+
+SELECT D.IDDEPARTAMENTO, D.NOMBRE, T.EMPS, T.PLANILLA
+FROM recursos.DEPARTAMENTO D
+JOIN (
+	SELECT IDDEPARTAMENTO, count(1) EMPS, sum(sueldo) PLANILLA
+	FROM recursos.EMPLEADO
+	GROUP BY IDDEPARTAMENTO ) T
+ON D.IDDEPARTAMENTO = T.IDDEPARTAMENTO;
 
 
+-- T3
+-- Se necesita una consulta que permita obtener
+-- la información resumen de el avance de las 
+-- matriculas, del importe proyectado, el importe 
+-- comprometido y el importe recaudado
+
+/*
+
+CODIGO     NOMBRE                   IMPORTE     IMPORTE
+CURSO      CURSO    MATRICULADOS    PROYECTADO  COMPROMETIDO   RECAUDADO
+------------------------------------------------------------------------------
+
+
+
+------------------------------------------------------------------------------
+
+*/
 
